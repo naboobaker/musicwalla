7ef16332f2c679568981ebd539a893bc4882caa1
Nadeem Aboobaker <nadeem.aboobaker@yahoo.com>
2012-06-30 22:59:28 -0700

    Use a prefix for ID, to fix bug in removal of Playlist Queue row.

--- app/assets/javascripts/playlist_queues.js
+++ app/assets/javascripts/playlist_queues.js
@@ -73,26 +73,20 @@ function playlistQueues_playlistQueuesCreate(playlistQueueId) {
 }
 
 function playlistQueues_playlistQueuesDestroy(playlistQueueId) {
-    var a = $("#playlist_queues");
-    var b = $("table#playlist_queues");
-    var c = $("#playlist_queues tr");
-    var d = $("table#playlist_queues tr");
-    var e = $("#playlist_queues #id" + playlistQueueId);
-    var f = $("table#playlist_queues #id" + playlistQueueId);
-    var g = $("#playlist_queues tr#id" + playlistQueueId);
-    var h = $("table#playlist_queues tr#id" + playlistQueueId);
-    var i = $("table#playlist_queues tbody tr#id" + playlistQueueId);
+    // since there are multiple ways to get to this method, need to make sure
+    // the Playlist Queue is still in the table - it could have already been removed
+    var playlistQueue = $("#playlist_queues tbody #id" + playlistQueueId);
+    if (playlistQueue.length > 0) {
+        // remove deleted playlist_queue
+        playlistQueue.remove();
 
+        // update row numbers, since a middle row could have been deleted
+        $("#playlist_queues tbody tr").each(function() {
+            $this = $(this)
+            var row = $this[0].rowIndex;
+            $this.find("td.row_number").html(row);
+        });
 
-    // remove deleted playlist_queue
-    $("#playlist_queues #id" + playlistQueueId).remove();
-
-    // update row numbers, since a middle row could have been deleted
-    $("#playlist_queues tbody tr").each(function() {
-        $this = $(this)
-        var row = $this[0].rowIndex;
-        $this.find("td.row_number").html(row);
-    });
-
-    playlistQueues_renderTableEmptyMessage();
+        playlistQueues_renderTableEmptyMessage();
+    }
 }
