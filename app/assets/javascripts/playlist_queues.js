497de6fabdc2a92376fca1d9dfd3ad7552d25553
Nadeem Aboobaker <nadeem.aboobaker@yahoo.com>
2012-06-30 21:56:27 -0700

    Second attempt at fixing bug in removal of Playlist Queue row.

--- app/assets/javascripts/playlist_queues.js
+++ app/assets/javascripts/playlist_queues.js
@@ -73,20 +73,15 @@ function playlistQueues_playlistQueuesCreate(playlistQueueId) {
 }
 
 function playlistQueues_playlistQueuesDestroy(playlistQueueId) {
-    // since there are multiple ways to get to this method, need to make sure
-    // the Playlist Queue is still in the table - it could have already been removed
-    var playlistQueue = $("#playlist_queues #" + playlistQueueId);
-    if (playlistQueue.length > 0) {
-        // remove deleted playlist_queue
-        playlistQueue.remove();
+    // remove deleted playlist_queue
+    $("#playlist_queues #" + playlistQueueId).remove();
 
-        // update row numbers, since a middle row could have been deleted
-        $("#playlist_queues tbody tr").each(function() {
-            $this = $(this)
-            var row = $this[0].rowIndex;
-            $this.find("td.row_number").html(row);
-        });
+    // update row numbers, since a middle row could have been deleted
+    $("#playlist_queues tbody tr").each(function() {
+        $this = $(this)
+        var row = $this[0].rowIndex;
+        $this.find("td.row_number").html(row);
+    });
 
-        playlistQueues_renderTableEmptyMessage();
-    }
+    playlistQueues_renderTableEmptyMessage();
 }
