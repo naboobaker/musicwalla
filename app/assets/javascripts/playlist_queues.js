69e7986072a9b902437740fe78ddfaf752a3f8fb
Nadeem Aboobaker <nadeem.aboobaker@yahoo.com>
2012-06-30 22:38:24 -0700

    Add statements to help debug bug in removal of Playlist Queue row.

--- app/assets/javascripts/playlist_queues.js
+++ app/assets/javascripts/playlist_queues.js
@@ -73,6 +73,15 @@ function playlistQueues_playlistQueuesCreate(playlistQueueId) {
 }
 
 function playlistQueues_playlistQueuesDestroy(playlistQueueId) {
+    var a = $("#playlist_queues");
+    var b = $("table#playlist_queues");
+    var c = $("#playlist_queues tr");
+    var d = $("table#playlist_queues tr");
+    var e = $("#playlist_queues #" + playlistQueueId);
+    var f = $("table#playlist_queues #" + playlistQueueId);
+    var g = $("#playlist_queues tr#" + playlistQueueId);
+    var h = $("table#playlist_queues tr#" + playlistQueueId);
+
     // remove deleted playlist_queue
     $("#playlist_queues #" + playlistQueueId).remove();
 
